ae1476c2de9d6450f0a7408586f61232be063bc7
ricardojava <ricardobatistaalmeida@gmail.com>
2016-06-29 16:25:06 -0300

    commit first

---
+++ src/main/java/com/avianca/RefundConfirmationApplication.java
@@ -0,0 +1,12 @@
+package com.avianca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefundConfirmationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefundConfirmationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/avianca/RefundConfirmationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.avianca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RefundConfirmationApplication.class)
+public class RefundConfirmationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
